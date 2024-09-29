6501730627839c381150ffa233731fa915d9b774
Pinki Parmar <ParmarPinki@outlook.com>
2024-09-29 16:07:39 +0530

    Update models.py

--- bookmymeal/models.py
+++ bookmymeal/models.py
@@ -2,7 +2,7 @@
 import mysql.connector
 
 # db=MySQLdb.connect('localhost','root','root','myproject3-430')
-db=mysql.connector.connect(host='localhost',database='register',user='arjungawali',password='08Oct2000')
+db=mysql.connector.connect(host='localhost',database='register',user='pinkiparmar',password='08Oct2000')
 
 cursor=db.cursor()
 print("connection done....")
